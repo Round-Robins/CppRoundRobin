69a63cf1aa8e08d1bb7380c943b8bc40fc73a705
Lucas Brendel <lucasbrendel@gmail.com>
2016-08-17 21:58:39 -0500

    adding first test files

---
+++ test/RoundRobinTest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lucas on 8/17/2016.
+//
+
+#include "RoundRobinTest.h"
---
+++ test/RoundRobinTest.h
@@ -0,0 +1,17 @@
+//
+// Created by lucas on 8/17/2016.
+//
+
+#ifndef CPPROUNDROBIN_ROUNDROBINTEST_H
+#define CPPROUNDROBIN_ROUNDROBINTEST_H
+
+#include <gtest/gtest.h>
+
+using namespace ::testing::test;
+
+class RoundRobinTest {
+
+};
+
+
+#endif //CPPROUNDROBIN_ROUNDROBINTEST_H
