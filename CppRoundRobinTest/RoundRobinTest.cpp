500abe795da8a8cd3cddfa0da285ef0b3efa662c
Lucas Brendel <lucasbrendel@gmail.com>
2016-09-07 20:44:21 -0500

    adding test file

---
+++ CppRoundRobinTest/RoundRobinTest.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include <RoundRobinTask.h>
+
+using namespace CppRoundRobin;
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CppRoundRobinTest
+{
+    TEST_CLASS(RoundRobinTests)
+    {
+    public:
+
+        TEST_METHOD(Setup)
+        {
+
+        }
+
+    };
+}
