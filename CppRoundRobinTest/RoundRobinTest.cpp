72d4e945b7eced0fc3f4cca3c98667b439839b79
Lucas Brendel <lucasbrendel@gmail.com>
2016-09-17 20:55:12 -0500

    random changes

--- CppRoundRobinTest/RoundRobinTest.cpp
+++ CppRoundRobinTest/RoundRobinTest.cpp
@@ -9,41 +9,58 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace CppRoundRobinTest
 {
-    TEST_CLASS(RoundRobinTests)
-    {
-    public:
+	TEST_CLASS(RoundRobinTests)
+	{
+	public:
 
-        TEST_METHOD(GetPeriod)
-        {
-            RoundRobin robin(1);
+		TEST_METHOD(GetPeriod)
+		{
+			RoundRobin robin(1);
 
-            Assert::AreEqual(robin.GetPeriod(), 1);
-        }
+			Assert::AreEqual(robin.GetPeriod(), 1);
+		}
 
-        TEST_METHOD(SetPeriod)
-        {
-            RoundRobin robin(5);
-            robin.SetPeriod(6);
+		TEST_METHOD(SetPeriod)
+		{
+			RoundRobin robin(5);
+			robin.SetPeriod(6);
 
-            Assert::AreEqual(robin.GetPeriod(), 6);
-        }
+			Assert::AreEqual(robin.GetPeriod(), 6);
+		}
 
-        TEST_METHOD(RunSingleTask)
-        {
-            RoundRobin robin(1);
+		TEST_METHOD(RunSingleTask)
+		{
+			RoundRobin robin(1);
 
-            std::shared_ptr<RoundRobinTask> task(new ExceptionThrowingRoundRobinTask(1));
-            robin.AddTask(task);
+			std::shared_ptr<RoundRobinTask> task(new ExceptionThrowingRoundRobinTask(1));
+			robin.AddTask(task);
 
-            try
-            {
-                robin.TimerFired(); // force fire the timer
-                robin.Run();
-            }
-            catch (int e)
-            {
-                Assert::AreEqual(30, e);
-            }
-        }
-    };
+			try
+			{
+				robin.TimerFired(); // force fire the timer
+				robin.Run();
+			}
+			catch (int e)
+			{
+				Assert::AreEqual(30, e);
+			}
+		}
+
+		TEST_METHOD(RunMultipleTasks)
+		{
+			RoundRobin robin(1);
+
+			std::shared_ptr<RoundRobinTask> task(new ExceptionThrowingRoundRobinTask(1000));
+			robin.AddTask(task);
+
+			try
+			{
+				robin.Run();
+			}
+			catch (int e)
+			{
+				Assert::AreEqual(30, e);
+			}
+		}
+	};
 }
